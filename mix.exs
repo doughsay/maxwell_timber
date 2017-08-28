eb573f89bc4ad7919fdf19b54e35758b7d0e6ea1
Chris Dosé <chris.dose@gmail.com>
2017-08-28 13:33:23 -0700

    Update versions for 0.4.0 release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MaxwellTimber.Mixfile do
 
   def project do
     [app: :maxwell_timber,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
